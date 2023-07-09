7c7c4a4dfd6f3c690fad343c7d558da821d861f5
Victor Borzov <borzov.vk@protonmail.com>
2023-07-09 19:37:34 +0200

    feat(nix-dir-env): added direnv example to explore later

--- configuration.nix
+++ configuration.nix
@@ -80,18 +80,11 @@
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
 
-  ## For mount.cifs, required unless domain name resolution is not needed.
-  #environment.systemPackages = [ pkgs.cifs-utils ];
-  #fileSystems."/mnt/ap-storage-algs" = {
-      #device = "//ap-team.ru/storage/Development/Teams/ALGS";
-      #fsType = "cifs";
-      #options = let
-        ## this line prevents hanging on network split
-        #automount_opts = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s";
-#
-      #in ["${automount_opts},credentials=/etc/nixos/smb-secrets"];
-  #};
-
+  # nix-direnv
+  nix.extraOptions = ''
+    keep-outputs = true
+    keep-derivations = true
+  '';
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
