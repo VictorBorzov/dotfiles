1672869e16dd44855311de20606b3ae8f4f6bee0
Victor Borzov <borzov.vk@protonmail.com>
2023-07-09 19:38:48 +0200

    fix(nixos): docker turned off

--- configuration.nix
+++ configuration.nix
@@ -106,7 +106,8 @@
   # networking.firewall.allowedUDPPorts = [ ... ];
   # Or disable the firewall altogether.
   # networking.firewall.enable = false;
-  virtualisation.docker.enable = true;
+
+  # virtualisation.docker.enable = true;
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
