85ec41e1a76bbf3f12d9ab58a310843d84009530
Victor Borzov <borzov.vk@protonmail.com>
2024-06-03 20:39:43 +0200

    feat: Hyprland bindings

--- home/hyprland/binds.nix
+++ home/hyprland/binds.nix
@@ -45,7 +45,7 @@ let
     let ws = let c = (x + 1) / 10; in builtins.toString (x + 1 - (c * 10));
     in [
       "$mod, ${ws}, workspace, ${toString (x + 1)}"
-      # "$mod, ${ws}, moveworkspacetomonitor, ${toString (x + 1)}"
+      "$mod, ${ws}, moveworkspacetomonitor, ${toString (x + 1)}"
       "$mod SHIFT, ${ws}, movetoworkspace, ${toString (x + 1)}"
     ]) 10);
 in {
