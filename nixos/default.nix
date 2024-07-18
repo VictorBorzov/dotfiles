7f7ee5c059ea2255a5538dee449d1df841fcf0e7
Victor Borzov <borzov.vk@protonmail.com>
2024-07-18 12:01:51 +0200

    update: Flake update

--- nixos/default.nix
+++ nixos/default.nix
@@ -60,7 +60,7 @@
   # hardware.opentabletdriver.enable = true;
 
   # Enable sound with pipewire.
-  sound.enable = true;
+  # sound.enable = true;
 
   hardware.pulseaudio.enable = false;
   security.rtkit.enable = true;
